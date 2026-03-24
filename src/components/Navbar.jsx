53c9154a13e703ac934139362d0248423e55743a
1-Ujjwal <kingayushman7@gmail.com>
2026-03-24 22:39:21 +0530

    Made navbar responsive and added food.js file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,19 @@ import { LuShoppingBag } from "react-icons/lu";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between w-full h-25 px-8">
+    <div className="flex items-center justify-between w-full h-25 px-5 md:px-8">
       <div className="flex items-center justify-center bg-white w-15 h-15 rounded-md shadow-xl">
         <MdFastfood className="text-green-500 w-7.5 h-7.5" />
       </div>
       <form
         action=""
-        className="flex items-center justify-center bg-white w-[60%] h-15 px-5 gap-5"
+        className="flex items-center justify-center bg-white w-[45%] h-15 px-5 gap-5 rounded-md shadow-md md:w-[70%]"
       >
         <IoSearch className="text-green-500 w-5 h-5 rounded-md shadow-xl" />
         <input
           type="text"
           placeholder="Search Items..."
-          className="w-full outline-none text-[20px]"
+          className="w-full outline-none text-[16px] md:text-[20px]"
         />
       </form>
       <div className="flex items-center justify-center bg-white w-15 h-15 rounded-md shadow-xl relative">
