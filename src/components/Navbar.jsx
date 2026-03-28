060bfa93981a7e65f2eefbdce066cd9e66650220
1-Ujjwal <kingayushman7@gmail.com>
2026-03-28 23:19:44 +0530

    Working Search Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { MdFastfood } from "react-icons/md";
 import { IoSearch } from "react-icons/io5";
 import { LuShoppingBag } from "react-icons/lu";
+import { dataContext } from "../context/UserContext";
+import { food_items } from "../food";
 
 const Navbar = () => {
+  let { input, setInput, cate, setCate } = useContext(dataContext);
+  useEffect(() => {
+    let newList = food_items.filter(
+      (item) =>
+        item.food_name.includes(input) ||
+        item.food_name.toLowerCase().includes(input),
+    );
+    setCate(newList);
+  }, [input]);
   return (
     <div className="flex items-center justify-between w-full h-25 px-5 md:px-8">
       <div className="flex items-center justify-center bg-white w-15 h-15 rounded-md shadow-xl">
@@ -12,12 +23,15 @@ const Navbar = () => {
       <form
         action=""
         className="flex items-center justify-center bg-white w-[45%] h-15 px-5 gap-5 rounded-md shadow-md md:w-[70%]"
+        onSubmit={(e) => e.preventDefault()}
       >
         <IoSearch className="text-green-500 w-5 h-5 rounded-md shadow-xl" />
         <input
           type="text"
           placeholder="Search Items..."
           className="w-full outline-none text-[16px] md:text-[20px]"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
         />
       </form>
       <div className="flex items-center justify-center bg-white w-15 h-15 rounded-md shadow-xl relative">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import Navbar from "../components/Navbar";
 import Categories from "../Categories";
 import Card from "../components/Card";
 import { food_items } from "../food";
+import { dataContext } from "../context/UserContext";
 
 const Home = () => {
-  let [cate, setCate] = useState(food_items);
+  let { cate, setCate, input } = useContext(dataContext);
 
   function filter(category) {
     if (category === "All") {
@@ -20,19 +21,22 @@ const Home = () => {
   return (
     <div className="bg-slate-200 w-full min-h-screen">
       <Navbar />
-      <div className="flex flex-wrap justify-center items-center gap-5 w-full">
-        {Categories.map((item) => {
-          return (
-            <div
-              className="w-35 h-37.5 bg-white flex flex-col items-start gap-5 p-5 justify-start text-[20px] font-semibold text-gray-600 rounded-lg shadow-xl hover:bg-green-200 cursor-pointer transition-all duration-200"
-              onClick={() => filter(item.name)}
-            >
-              {item.icon}
-              {item.name}
-            </div>
-          );
-        })}
-      </div>
+      {!input && (
+        <div className="flex flex-wrap justify-center items-center gap-5 w-full">
+          {Categories.map((item) => {
+            return (
+              <div
+                className="w-35 h-37.5 bg-white flex flex-col items-start gap-5 p-5 justify-start text-[20px] font-semibold text-gray-600 rounded-lg shadow-xl hover:bg-green-200 cursor-pointer transition-all duration-200"
+                onClick={() => filter(item.name)}
+              >
+                {item.icon}
+                {item.name}
+              </div>
+            );
+          })}
+        </div>
+      )}
+
       <div className="w-full flex flex-wrap gap-5 px-5 justify-center items-center pt-8 pb-8">
         {cate.map((item) => (
           <Card
